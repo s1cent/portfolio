f832a1ddf995f2593cbee0f83b5d9a6aaea348ba
s1cent <44167062+s1cent@users.noreply.github.com>
2023-03-29 22:55:16 +0200

    updating Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FirstParagraphComponent } from './home-page/first-paragraph/first-paragraph.component';
+import { SmallProjectsComponent } from './home-page/small-projects/small-projects.component';
+import { SkillsComponent } from './home-page/skills/skills.component';
+import { ContactsComponent } from './home-page/contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FirstParagraphComponent,
+    SmallProjectsComponent,
+    SkillsComponent,
+    ContactsComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
